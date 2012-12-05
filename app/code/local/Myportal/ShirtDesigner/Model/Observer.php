90ace86ca51f370ac4fbe2c78cbfe94f982919f7
HaifaCarina <haifa@baluyos.net>
2012-12-06 05:35:10 +0800

    Added change price action in observer

--- app/code/local/Myportal/ShirtDesigner/Model/Observer.php
+++ app/code/local/Myportal/ShirtDesigner/Model/Observer.php
@@ -5,11 +5,15 @@ class Myportal_ShirtDesigner_Model_Observer
 	   public function hookToAddToCartBefore($observer)
 	   {
                Mage::log('hookToAddToCartBefore event called');
-               echo '<script type="text/javascript">alert("stopWhenAddingToCartOKAAAAY!");</script>';
-	   /*      $event = $observer->getEvent();
-	         $model = $event->getPage();
-	     		print_r($model->getData());
-	         *///die('test');
+               $event = $observer->getEvent();
+               $quote_item = $event->getQuoteItem();
+               $price = $quote_item->getProduct()->getPrice();
+                
+               $new_price = Mage::getSingleton('core/session')->getNewPrice();
+               
+               $quote_item->setOriginalCustomPrice($new_price);
+               $quote_item->save();
+           
 	 } 
 } 
 ?>
